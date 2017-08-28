7bd14b2c2d44752ac00df66527fb3c705e8d2bb1
Daniel Lin <linzhdk@gmail.com>
2017-08-28 20:56:10 +0800

    Add TextField Node

---
+++ NodeExtension/Classes/Node/DLTextFieldNode.swift
@@ -0,0 +1,193 @@
+//
+//  DLTextFieldNode.swift
+//  NodeExtension
+//
+//  Created by Daniel Lin on 28/08/2017.
+//  Copyright (c) 2017 Daniel Lin. All rights reserved.
+//
+
+import AsyncDisplayKit
+
+/// The Node object of text field view with right control node
+open class DLTextFieldNode: ASDisplayNode {
+
+    public let originalNode: DLSimpleTextFieldNode
+    public var rightButtonNode: ASButtonNode?
+    
+    public var insideInsets: UIEdgeInsets {
+        get {
+            return originalNode.insets
+        }
+        set {
+            originalNode.insets = newValue
+        }
+    }
+    public var outsideInsets = UIEdgeInsets.zero
+    
+    public var isValidated: Bool {
+        return originalNode.isValidated
+    }
+    
+    public var text: String? {
+        get {
+            return self.originalNode.text
+        }
+        set {
+            self.originalNode.text = newValue
+        }
+    }
+    
+    public init(iconName: String? = nil, placeholder: String, maxLength: UInt = 32, isSecure: Bool = false, clearMode: UITextFieldViewMode = .always, keyboardType: UIKeyboardType? = nil, returnKeyType: UIReturnKeyType? = nil) {
+        originalNode = DLSimpleTextFieldNode(iconName: iconName, placeholder: placeholder, maxLength: maxLength, isSecure: isSecure, clearMode: clearMode, keyboardType: keyboardType, returnKeyType: returnKeyType)
+        super.init()
+        
+        self.automaticallyManagesSubnodes = true
+    }
+    
+    override open func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        var children = [ASLayoutElement](arrayLiteral: originalNode)
+        if let rightButtonNode = rightButtonNode {
+            rightButtonNode.style.spacingAfter = outsideInsets.right
+            children.append(rightButtonNode)
+        } else {
+            originalNode.style.spacingAfter = outsideInsets.right / 2
+        }
+        let horizontalStackSpec = ASStackLayoutSpec(direction: .horizontal, spacing: insideInsets.right, justifyContent: .start, alignItems: .center, children: children)
+        return ASInsetLayoutSpec(insets: UIEdgeInsets(top: outsideInsets.top, left: outsideInsets.left, bottom: outsideInsets.bottom, right: 0), child: horizontalStackSpec)
+    }
+    
+    override open func becomeFirstResponder() -> Bool {
+        return originalNode.becomeFirstResponder()
+    }
+    
+    override open func resignFirstResponder() -> Bool {
+        return originalNode.resignFirstResponder()
+    }
+}
+
+/// The Node objcet of simple text field view
+open class DLSimpleTextFieldNode: DLViewNode<DLTextField> {
+    
+    public var isSecureTextEntry: Bool {
+        get {
+            return self.nodeView.isSecureTextEntry
+        }
+        set {
+            self.appendViewAssociation { (view: DLTextField) in
+                view.isSecureTextEntry = newValue
+            }
+        }
+    }
+    
+    public var text: String? {
+        get {
+            return self.nodeView.text
+        }
+        set {
+            self.appendViewAssociation { (view: DLTextField) in
+                view.text = newValue
+            }
+        }
+    }
+    
+    public var textColor: UIColor? {
+        get {
+            return self.nodeView.textColor
+        }
+        set {
+            self.appendViewAssociation { (view: DLTextField) in
+                view.textColor = newValue
+            }
+        }
+    }
+    
+    public var textSize: CGFloat? {
+        didSet {
+            let _textSize = textSize!
+            self.appendViewAssociation { (view: DLTextField) in
+                view.font = UIFont.systemFont(ofSize: _textSize)
+            }
+        }
+    }
+    
+    public var insets: UIEdgeInsets {
+        get {
+            return self.nodeView.insets
+        }
+        set {
+            self.appendViewAssociation { (view: DLTextField) in
+                view.insets = newValue
+            }
+        }
+    }
+    
+    public var isValidated: Bool {
+        var isValidated = validation()
+        if isValidated, let appendedValidation = _appendedValidation {
+            isValidated = isValidated && appendedValidation()
+        }
+        if !isValidated {
+            _ = self.becomeFirstResponder()
+        }
+        return isValidated
+    }
+    
+    open var validation: () -> Bool {
+        return { () -> Bool in
+            guard let text = self.text, !text.isEmpty else {
+                return false
+            }
+            
+            return true
+        }
+    }
+    
+    private var _appendedValidation: (() -> Bool)?
+    
+    public init(iconName: String? = nil, placeholder: String, maxLength: UInt = 32, isSecure: Bool = false, clearMode: UITextFieldViewMode = .always, keyboardType: UIKeyboardType? = nil, returnKeyType: UIReturnKeyType? = nil) {
+        super.init()
+        
+        self.setViewBlock { () -> UIView in
+            let textField = DLTextField()
+            
+            if let iconName = iconName {
+                textField.leftView = UIImageView(image: UIImage.as_imageNamed(iconName))
+                textField.leftViewMode = .always
+            }
+            
+            textField.placeholder = placeholder
+            textField.maxLength = maxLength
+            
+            textField.clearButtonMode = clearMode
+            if isSecure {
+                textField.clearButtonMode = .never
+                textField.isSecureTextEntry = true
+                textField.keyboardAppearance = .dark
+            }
+            
+            if let keyboardType = keyboardType {
+                textField.keyboardType = keyboardType
+            }
+            
+            if let returnKeyType = returnKeyType {
+                textField.returnKeyType = returnKeyType
+            }
+            return textField
+        }
+        self.style.alignSelf = .stretch
+        self.style.flexGrow = 1.0
+        self.style.flexShrink = 1.0
+    }
+    
+    override open func becomeFirstResponder() -> Bool {
+        return self.nodeView.becomeFirstResponder()
+    }
+    
+    override open func resignFirstResponder() -> Bool {
+        return self.nodeView.resignFirstResponder()
+    }
+    
+    public func appendValidation(_ block: @escaping () -> Bool) {
+        _appendedValidation = block
+    }
+}
---
+++ NodeExtension/Classes/UIKit/DLTextField.swift
@@ -0,0 +1,102 @@
+//
+//  DLTextField.swift
+//  NodeExtension
+//
+//  Created by Daniel Lin on 28/08/2017.
+//  Copyright (c) 2017 Daniel Lin. All rights reserved.
+//
+
+import UIKit
+
+/// The basic view of TextField
+open class DLTextField: UITextField {
+
+    public var insets = UIEdgeInsets.zero
+    
+    public var maxLength: UInt? {
+        didSet {
+            if maxLength == 0 {
+                return
+            }
+            
+            if !_didAddObserver {
+                _didAddObserver = true
+                NotificationCenter.default.addObserver(self, selector: #selector(_textFieldDidChange(notification:)), name: NSNotification.Name.UITextFieldTextDidChange, object: self)
+            }
+        }
+    }
+
+    private var _didAddObserver = false
+    
+    deinit {
+        if _didAddObserver {
+            NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UITextFieldTextDidChange, object: self)
+        }
+    }
+    
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return adjustRectForLeftView(bounds: bounds)
+    }
+    
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return adjustRectForLeftView(bounds: bounds)
+    }
+    
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return adjustRectForLeftView(bounds: bounds)
+    }
+    
+    private func adjustRectForLeftView(bounds: CGRect) -> CGRect {
+        var bounds = UIEdgeInsetsInsetRect(bounds, insets)
+        
+        if let leftView = leftView {
+            bounds.origin.x += leftView.frame.width
+            bounds.size.width -= leftView.frame.width
+        }
+        
+        if let rightView = rightView {
+            bounds.size.width -= rightView.frame.width
+        }
+        
+        if self.clearButtonMode != .never {
+            bounds.size.width -= insets.right
+        }
+        
+        return bounds
+    }
+    
+    @objc private func _textFieldDidChange(notification: Notification) {
+        let lang = (notification.object as! UIResponder).textInputMode?.primaryLanguage
+        if lang == "zh-Hans" {
+            guard let selectedRange = self.markedTextRange else {
+                substringWithinMaxLength()
+                return
+            }
+            
+            if self.position(from: selectedRange.start, offset: 0) == nil {
+                substringWithinMaxLength()
+                return
+            }
+            
+        } else {
+            substringWithinMaxLength()
+        }
+    }
+    
+    private func substringWithinMaxLength() {
+        guard let text = self.text as NSString? else {
+            return
+        }
+        
+        let maxLength = Int(self.maxLength!)
+        if text.length > maxLength {
+            let rangeIndex = text.rangeOfComposedCharacterSequence(at: maxLength)
+            if rangeIndex.length == 1 {
+                self.text = text.substring(to: maxLength)
+            } else {
+                let range = text.rangeOfComposedCharacterSequences(for: NSMakeRange(0, maxLength))
+                self.text = text.substring(with: range)
+            }
+        }
+    }
+}
