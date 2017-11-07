cdc58075eee9b03ef75262ff32eae102064e752e
Daniel Lin <linzhdk@gmail.com>
2017-11-07 13:06:01 +0800

    Add enum of device screen size with inch

--- NodeExtension/Classes/UIKit/Constants.swift
+++ NodeExtension/Classes/UIKit/Constants.swift
@@ -22,6 +22,35 @@ public struct DLFrameSize {
     
 }
 
+public enum DLScreenInch {
+    case iPhone_3_5
+    case iPhone_4
+    case iPhone_4_7
+    case iPhone_5_5
+    case iPhone_5_8
+    case unspecified
+    
+    public static func currentDevice() -> DLScreenInch {
+        if UIDevice().userInterfaceIdiom != .phone {
+            return .unspecified
+        }
+        
+        if UIScreen.main.nativeBounds.width == 640 && UIScreen.main.nativeBounds.height == 960 {
+            return .iPhone_3_5
+        } else if UIScreen.main.nativeBounds.width == 640 && UIScreen.main.nativeBounds.height == 1136 {
+            return .iPhone_4
+        } else if UIScreen.main.nativeBounds.width == 750 && UIScreen.main.nativeBounds.height == 1334 {
+            return .iPhone_4_7
+        } else if UIScreen.main.nativeBounds.width == 1242 && UIScreen.main.nativeBounds.height == 2208 {
+            return .iPhone_5_5
+        } else if UIScreen.main.nativeBounds.width == 1125 && UIScreen.main.nativeBounds.height == 2436 {
+            return .iPhone_5_8
+        } else {
+            return .unspecified
+        }
+    }
+}
+
 extension UIColor {
     
     /// Tint color of application window
