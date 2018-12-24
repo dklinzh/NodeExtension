56927cb6e869b4893808442827d3e07d3919b69f
Daniel Lin <linzhdk@gmail.com>
2018-12-24 11:01:32 +0800

    Update extension functions of MBProgressHUD

--- NodeExtension/Classes/UIKit/HUD/ProgressHUD.swift
+++ NodeExtension/Classes/UIKit/HUD/ProgressHUD.swift
@@ -19,58 +19,7 @@ extension MBProgressHUD {
     public static var dl_failureView: UIView?
     public static var dl_successDismissedDelay: TimeInterval = 2
     public static var dl_failureDismissedDelay: TimeInterval = 3
-    
-    /// Only show text on the view of current UIViewController.
-    ///
-    /// - Parameter text: The text string to show
-    public static func dl_showOnlyText(_ text: String?) {
-        guard let superView = UIViewController.dl_topView() else {
-            return
-        }
-        
-        dl_showOnlyText(text, superView: superView)
-    }
-    
-    /// Only show text on the super view
-    ///
-    /// - Parameters:
-    ///   - text: The text string to show
-    ///   - superView: The super view of hud
-    public static func dl_showOnlyText(_ text: String?, superView: UIView) {
-        guard let text = text, !text.isEmpty else {
-            return
-        }
-        
-        let hud = MBProgressHUD.showAdded(to: superView, animated: true)
-        hud.mode = .text
-        hud.label.text = text
-        hud.label.textColor = dl_textColor
-        hud.label.font = UIFont.systemFont(ofSize: dl_textSize)
-        //        hud.offset = CGPoint(x: 0, y: superView.bounds.size.height/3.0)
-        hud.margin = dl_contentMargin
-        hud.bezelView.backgroundColor = dl_backgroundColor
-        hud.hide(animated: true, afterDelay: 2)
-    }
-    
-    /// Show an indicator on the view of current UIViewController
-    public static func dl_showIndicator(isOverlayInteractionEnabled: Bool = true) {
-        guard let superView = UIViewController.dl_topView() else {
-            return
-        }
-        
-        dl_showIndicator(superView: superView, isOverlayInteractionEnabled: isOverlayInteractionEnabled)
-    }
-    
-    /// Show an indicator on the super view
-    ///
-    /// - Parameter superView: The super view of hud
-    public static func dl_showIndicator(superView: UIView, isOverlayInteractionEnabled: Bool = true) {
-        let hud = MBProgressHUD.showAdded(to: superView, animated: true)
-        hud.mode = .indeterminate
-        hud.margin = dl_contentMargin
-        hud.bezelView.backgroundColor = dl_backgroundColor
-        hud.isUserInteractionEnabled = !isOverlayInteractionEnabled
-    }
+    public static var dl_graceTime: TimeInterval = 0
     
     /// Hide the indicator have been shown on the view of current UIViewController
     public static func dl_hideHUD() {
@@ -88,15 +37,78 @@ extension MBProgressHUD {
         MBProgressHUD.hide(for: superView, animated: true)
     }
     
+    /// Only show text on the view of current UIViewController.
+    ///
+    /// - Parameter text: The text string to show
+    @discardableResult
+    public static func dl_showOnlyText(_ text: String?, completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD? {
+        guard let superView = UIViewController.dl_topView() else {
+            return nil
+        }
+        
+        return dl_showOnlyText(text, superView: superView, completion: completion)
+    }
+    
+    /// Only show text on the super view
+    ///
+    /// - Parameters:
+    ///   - text: The text string to show
+    ///   - superView: The super view of hud
+    @discardableResult
+    public static func dl_showOnlyText(_ text: String?, superView: UIView, completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD? {
+        guard let text = text, !text.isEmpty else {
+            return nil
+        }
+        
+        let hud = MBProgressHUD.showAdded(to: superView, animated: true)
+        hud.completionBlock = completion
+        hud.mode = .text
+        hud.label.text = text
+        hud.label.textColor = dl_textColor
+        hud.label.font = UIFont.systemFont(ofSize: dl_textSize)
+        //        hud.offset = CGPoint(x: 0, y: superView.bounds.size.height/3.0)
+        hud.margin = dl_contentMargin
+        hud.bezelView.backgroundColor = dl_backgroundColor
+        hud.hide(animated: true, afterDelay: 2)
+        hud.graceTime = dl_graceTime
+        return hud
+    }
+    
+    /// Show an indicator on the view of current UIViewController
+    @discardableResult
+    public static func dl_showIndicator(isOverlayInteractionEnabled: Bool = true, completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD? {
+        guard let superView = UIViewController.dl_topView() else {
+            return nil
+        }
+        
+        return dl_showIndicator(superView: superView, isOverlayInteractionEnabled: isOverlayInteractionEnabled, completion: completion)
+    }
+    
+    /// Show an indicator on the super view
+    ///
+    /// - Parameter superView: The super view of hud
+    @discardableResult
+    public static func dl_showIndicator(superView: UIView, isOverlayInteractionEnabled: Bool = true, completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD {
+        let hud = MBProgressHUD.showAdded(to: superView, animated: true)
+        hud.completionBlock = completion
+        hud.mode = .indeterminate
+        hud.margin = dl_contentMargin
+        hud.bezelView.backgroundColor = dl_backgroundColor
+        hud.isUserInteractionEnabled = !isOverlayInteractionEnabled
+        hud.graceTime = dl_graceTime
+        return hud
+    }
+    
     /// Show success hud with message on the view of current UIViewController
     ///
     /// - Parameter text: The successful message
-    public static func dl_showSuccess(text: String?) {
+    @discardableResult
+    public static func dl_showSuccess(text: String?, completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD? {
         guard let superView = UIViewController.dl_topView() else {
-            return
+            return nil
         }
         
-        dl_showSuccess(text: text, superView: superView)
+        return dl_showSuccess(text: text, superView: superView, completion: completion)
     }
     
     /// Show success hud whit message on the super view
@@ -104,12 +116,14 @@ extension MBProgressHUD {
     /// - Parameters:
     ///   - text: The successful message
     ///   - superView: The super view of hud
-    public static func dl_showSuccess(text: String?, superView: UIView) {
+    @discardableResult
+    public static func dl_showSuccess(text: String?, superView: UIView, completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD? {
         guard let text = text, !text.isEmpty else {
-            return
+            return nil
         }
         
         let hud = MBProgressHUD.showAdded(to: superView, animated: true)
+        hud.completionBlock = completion
         hud.mode = .customView
         hud.customView = dl_successView ?? SuccessShapeView(color: dl_successColor)
         hud.label.text = text
@@ -118,17 +132,20 @@ extension MBProgressHUD {
         hud.margin = dl_contentMargin
         hud.bezelView.backgroundColor = dl_backgroundColor
         hud.hide(animated: true, afterDelay: dl_successDismissedDelay)
+        hud.graceTime = dl_graceTime
+        return hud
     }
     
     /// Show failure hud with message on the view of current UIViewController
     ///
     /// - Parameter text: The failed message
-    public static func dl_showFailure(text: String?) {
+    @discardableResult
+    public static func dl_showFailure(text: String?, completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD? {
         guard let superView = UIViewController.dl_topView() else {
-            return
+            return nil
         }
         
-        dl_showFailure(text: text, superView: superView)
+        return dl_showFailure(text: text, superView: superView, completion: completion)
     }
     
     /// Show failure hud whit message on the super view
@@ -136,12 +153,14 @@ extension MBProgressHUD {
     /// - Parameters:
     ///   - text: The failed message
     ///   - superView: The super view of hud
-    public static func dl_showFailure(text: String?, superView: UIView) {
+    @discardableResult
+    public static func dl_showFailure(text: String?, superView: UIView, completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD? {
         guard let text = text, !text.isEmpty else {
-            return
+            return nil
         }
         
         let hud = MBProgressHUD.showAdded(to: superView, animated: true)
+        hud.completionBlock = completion
         hud.mode = .customView
         hud.customView = dl_failureView ?? FailureShapeView(color: dl_failureColor)
         hud.label.text = text
@@ -150,51 +169,61 @@ extension MBProgressHUD {
         hud.margin = dl_contentMargin
         hud.bezelView.backgroundColor = dl_backgroundColor
         hud.hide(animated: true, afterDelay: dl_failureDismissedDelay)
+        hud.graceTime = dl_graceTime
+        return hud
     }
     
     /// Show a determinate progress on the view of current UIViewController
     ///
     /// - Returns: HUD instance
-    public static func dl_showDeterminateProgress() -> MBProgressHUD? {
+    @discardableResult
+    public static func dl_showDeterminateProgress(completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD? {
         guard let superView = UIViewController.dl_topView() else {
             return nil
         }
         
-        return dl_showDeterminateProgress(superView: superView)
+        return dl_showDeterminateProgress(superView: superView, completion: completion)
     }
     
     /// Show a determinate progress on the super view
     ///
     /// - Parameter superView: The super view of hud
     /// - Returns: HUD instance
-    public static func dl_showDeterminateProgress(superView: UIView) -> MBProgressHUD {
+    @discardableResult
+    public static func dl_showDeterminateProgress(superView: UIView, completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD {
         let hud = MBProgressHUD.showAdded(to: superView, animated: true)
+        hud.completionBlock = completion
         hud.mode = .determinate
         hud.margin = dl_contentMargin
         hud.bezelView.backgroundColor = dl_backgroundColor
+        hud.graceTime = dl_graceTime
         return hud
     }
     
     /// Show a annular progress on the view of current UIViewController
     ///
     /// - Returns: HUD instance
-    public static func dl_showAnnularProgress() -> MBProgressHUD? {
+    @discardableResult
+    public static func dl_showAnnularProgress(completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD? {
         guard let superView = UIViewController.dl_topView() else {
             return nil
         }
         
-        return dl_showAnnularProgress(superView: superView)
+        return dl_showAnnularProgress(superView: superView, completion: completion)
     }
     
     /// Show a annular progress on the super view
     ///
     /// - Parameter superView: The super view of hud
     /// - Returns: HUD instance
-    public static func dl_showAnnularProgress(superView: UIView) -> MBProgressHUD {
+    @discardableResult
+    public static func dl_showAnnularProgress(superView: UIView, completion: MBProgressHUDCompletionBlock? = nil) -> MBProgressHUD {
         let hud = MBProgressHUD.showAdded(to: superView, animated: true)
+        hud.completionBlock = completion
         hud.mode = .annularDeterminate
         hud.margin = dl_contentMargin
         hud.bezelView.backgroundColor = dl_backgroundColor
+        hud.graceTime = dl_graceTime
         return hud
     }
 }
