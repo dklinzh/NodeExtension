a525827f7e82a8df608ecfea451931dc409222cf
Daniel Lin <linzhdk@gmail.com>
2020-01-02 09:56:43 +0800

    fix(node): only create an `UISearchBar` without the `UISearchController` if parameter `searchResultsController` is nil

--- NodeExtension/Classes/Node/DLSearchBarNode.swift
+++ NodeExtension/Classes/Node/DLSearchBarNode.swift
@@ -5,17 +5,16 @@
 //  Created by Daniel Lin on 2018/12/4.
 //
 
-import UIKit
 import AsyncDisplayKit
+import UIKit
 
 open class DLSearchBarNode: DLViewNode<UISearchBar>, UISearchResultsUpdating, UISearchBarDelegate {
-    
     public typealias UpdateSearchResultsBlock = (_ searchController: UISearchController) -> Void
-    
+
     public typealias SearchButtonClickedBlock = (_ searchBar: UISearchBar) -> Void
-    
+
     public let searchResultsController: UIViewController?
-    
+
     public lazy var searchController: UISearchController = {
         let searchController = UISearchController(searchResultsController: searchResultsController)
         if #available(iOS 9.1, *) {
@@ -25,49 +24,56 @@ open class DLSearchBarNode: DLViewNode<UISearchBar>, UISearchResultsUpdating, UI
         }
         searchController.hidesNavigationBarDuringPresentation = true
         searchController.searchResultsUpdater = self
-        
+
         let searchBar = searchController.searchBar
         appearanceSetup(searchBar: searchBar)
         return searchController
     }()
-    
+
     open func appearanceSetup(searchBar: UISearchBar) {
         searchBar.searchBarStyle = .minimal
         searchBar.backgroundColor = .white
         searchBar.isTranslucent = false
         searchBar.delegate = self
     }
-    
+
     private var _updateSearchResultsBlock: UpdateSearchResultsBlock?
     private var _searchButtonClickedBlock: SearchButtonClickedBlock?
-    
+
     public init(searchResultsController: UIViewController? = nil) {
         self.searchResultsController = searchResultsController
         super.init()
-        
-        self.setViewBlock { [unowned self] () -> UIView in
-            return self.searchController.searchBar
+
+        setViewBlock { [unowned self] () -> UIView in
+            if searchResultsController == nil {
+                let searchBar = UISearchBar()
+                self.appearanceSetup(searchBar: searchBar)
+                return searchBar
+            } else {
+                return self.searchController.searchBar
+            }
         }
-        
-        self.style.minHeight = ASDimensionMake(44)
+
+        style.minHeight = ASDimensionMake(44.0)
     }
-    
+
     public func setUpdateSearchResultsBlock(_ block: @escaping UpdateSearchResultsBlock) {
         _updateSearchResultsBlock = block
     }
-    
+
     public func setSearchButtonClickedBlock(_ block: @escaping SearchButtonClickedBlock) {
         _searchButtonClickedBlock = block
     }
-    
-// MARK: - UISearchResultsUpdating
+
+    // MARK: - UISearchResultsUpdating
+
     open func updateSearchResults(for searchController: UISearchController) {
         _updateSearchResultsBlock?(searchController)
     }
-    
-// MARK: - UISearchBarDelegate
+
+    // MARK: - UISearchBarDelegate
+
     open func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         _searchButtonClickedBlock?(searchBar)
     }
-    
 }
