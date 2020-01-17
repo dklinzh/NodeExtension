c1cef3562ec35040a7e8f6a59a71435cd2f8c1aa
Daniel Lin <linzhdk@gmail.com>
2020-01-17 17:40:57 +0800

    fix(UIKit): update the extensions of `UINavigationBar`

--- NodeExtension/Classes/UIKit/Navigation.swift
+++ NodeExtension/Classes/UIKit/Navigation.swift
@@ -7,7 +7,6 @@
 //
 
 extension UINavigationBar {
-    
     /// Set some attributes for the specified navigation bar
     ///
     /// - Parameters:
@@ -15,28 +14,33 @@ extension UINavigationBar {
     ///   - tintColor: The tintColor of navigation bar
     ///   - titleColor: The title color of navigation bar
     ///   - titleSize: The title font size of navigaton bar
-    public func dl_setAttributes(barTintColor: UIColor = .white, tintColor: UIColor = .blue, titleColor: UIColor = .black, titleSize: CGFloat = 16.0) {
+    public func dl_setAttributes(barTintColor: UIColor = .white,
+                                 tintColor: UIColor = .blue,
+                                 titleColor: UIColor = .black,
+                                 titleSize: CGFloat = 16.0) {
         self.barTintColor = barTintColor
         self.tintColor = tintColor
-        self.titleTextAttributes = [NSAttributedString.Key.foregroundColor : titleColor,
+        self.titleTextAttributes = [NSAttributedString.Key.foregroundColor: titleColor,
                                     NSAttributedString.Key.font: UIFont.systemFont(ofSize: titleSize)]
     }
-    
-    /// Set the image of BackBarButtonItem
-    ///
-    /// - Parameter image: The image of BackBarButtonItem
-    public func dl_setBackBarButtonItem(image: UIImage) {
+
+    /// Set the image of BackBarButtonItem.
+    /// - Parameters:
+    ///   - image: The image of BackBarButtonItem.
+    ///   - mode: The rendering mode to use for the image.
+    public func dl_setBackBarButtonItem(image: UIImage, mode: UIImage.RenderingMode = .automatic) {
+        let image = image.withRenderingMode(mode)
         self.backIndicatorImage = image
         self.backIndicatorTransitionMaskImage = image
     }
-    
+
     /// Set alpha value of navigation bar background view. It would be transparent while alpha equals 0.
     ///
     /// - Parameter alpha: Range: (0, 1.0)
     public func dl_setBackgroundAlpha(_ alpha: CGFloat) {
         self.setValue(alpha, forKeyPath: "backgroundView.alpha")
     }
-    
+
     /// Hide the underline images under the navigation bar
     public func dl_hideBarShadowUnderline() {
         self.shadowImage = UIImage()
@@ -44,12 +48,12 @@ extension UINavigationBar {
     }
 }
 
-//public protocol DLNavigationControllerDelegate {
+// public protocol DLNavigationControllerDelegate {
 //
 //    func navigationConroller(_ navigationConroller: UINavigationController, shouldPop item: UINavigationItem) -> Bool
-//}
+// }
 //
-//extension UINavigationController: UINavigationBarDelegate {
+// extension UINavigationController: UINavigationBarDelegate {
 //
 //    public func navigationBar(_ navigationBar: UINavigationBar, shouldPop item: UINavigationItem) -> Bool {
 //        if let items = navigationBar.items,
@@ -77,4 +81,4 @@ extension UINavigationBar {
 //
 //        return false
 //    }
-//}
+// }
